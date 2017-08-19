39e9cf9724128bd6b0c3b049a5242d944d2a1b8c
kyu <kyu@172.30.1.1>
2017-08-19 15:39:13 +0900

    템플릿 추가 / 디폴트 컨트롤러 추가

---
+++ src/main/java/com/kyunam/springscheduler/SpringSchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam.springscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kyunam/springscheduler/SpringSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kyunam.springscheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSchedulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
